078e882da7f968bcf7df12125273bc7049564597
Japneet-KaurChawla <japneetkaurchawla231106@gmail.com>
2026-05-13 21:27:02 +0530

    add cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A small ML project to find the emotions from a given machine in the easiest and quickest possible way.',
+    author='Japneet Kaur',
+    license='',
+)
